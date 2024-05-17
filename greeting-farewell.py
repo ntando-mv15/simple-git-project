4722ed2fe04e03c7da8500200996ca5130dc9a96
ntando-mv15 <ntando.mv15@gmail.com>
2024-05-17 09:49:28 +0200

    Combined greeting and farewell

---
+++ greeting-farewell.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def farewell(name):
+    return f"Goodbye, {name}!"
+
+if __name__ == "__main__":
+    name = "Git User"
+    print(greet(name))
+    print(farewell(name))
+
